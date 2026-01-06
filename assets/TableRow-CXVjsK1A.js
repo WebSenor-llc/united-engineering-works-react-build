9f8a87783c328d4b47e650737ff2b2765cce8445
priyankawebsenor-dev <priyankawebsenor@gmail.com>
2026-01-06 13:32:40 +0530

    new update

---
+++ assets/TableRow-CXVjsK1A.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BSu8tAiZ.js";const s=({children:o})=>r.jsx("tr",{className:"bg-white hover:bg-gray-50 transition-colors duration-150 ease-in-out",children:o});export{s as T};
