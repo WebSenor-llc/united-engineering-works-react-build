5b4fb50d13136a1d71a72e479c2a3154710246a3
priyankawebsenor-dev <priyankawebsenor@gmail.com>
2026-01-15 13:09:41 +0530

    new update

--- assets/BackButton-DPTpGdDT.js
+++
@@ -1,2 +0,0 @@
-import{u as a,j as t,a7 as e}from"./index-t0IFwL8M.js";const n=()=>{const s=a();return t.jsx("button",{onClick:()=>s(-1),className:`flex items-center\r
-                 text-gray-700 rounded-lg mt-3 transition justify-start mr-2`,children:t.jsx(e,{className:"w-6 h-6"})})};export{n as B};
--- assets/TableRow-EWPC2dMz.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-t0IFwL8M.js";const s=({children:o})=>r.jsx("tr",{className:"bg-white hover:bg-gray-50 transition-colors duration-150 ease-in-out",children:o});export{s as T};
