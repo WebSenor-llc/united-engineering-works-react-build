55d597730a309a80c1933cb45585ff4541279f42
priyankawebsenor-dev <priyankawebsenor@gmail.com>
2026-01-17 13:35:39 +0530

    new

---
+++ assets/BackButton-DXTvHfQF.js
@@ -0,0 +1,2 @@
+import{u as a,j as t,a6 as e}from"./index-DTUaUizH.js";const n=()=>{const s=a();return t.jsx("button",{onClick:()=>s(-1),className:`flex items-center\r
+                 text-gray-700 rounded-lg mt-3 transition justify-start mr-2`,children:t.jsx(e,{className:"w-6 h-6"})})};export{n as B};
