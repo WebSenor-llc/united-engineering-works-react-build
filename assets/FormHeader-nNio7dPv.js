7733652f0b1d6cd4fc09772a8e6bbba64dee06bd
priyankawebsenor-dev <priyankawebsenor@gmail.com>
2026-01-20 10:13:22 +0530

    newupdate

--- assets/FormHeader-nNio7dPv.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-BEDO0dex.js";import{B as t}from"./BackButton-D3G9tbc7.js";const c=({title:r,subtitle:e})=>s.jsxs("div",{className:"space-y-1 flex ",children:[s.jsx("div",{children:s.jsx(t,{})}),s.jsxs("div",{children:[s.jsx("h1",{className:"text-2xl font-bold text-gray-800",children:r}),e&&s.jsx("p",{className:"text-gray-500 text-sm",children:e})]})]});export{c as F};
