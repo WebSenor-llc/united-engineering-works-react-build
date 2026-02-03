2cf9d3198e899f47be1a3a87cd476d1ffeea81f3
priyankawebsenor-dev <priyankawebsenor@gmail.com>
2026-02-03 11:18:38 +0530

    excel

--- assets/FormHeader-Cf55-c6p.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-5cgORhEY.js";import{B as t}from"./BackButton-C2V7tOX5.js";const c=({title:r,subtitle:e})=>s.jsxs("div",{className:"space-y-1 flex ",children:[s.jsx("div",{children:s.jsx(t,{})}),s.jsxs("div",{children:[s.jsx("h1",{className:"text-2xl font-bold text-gray-800",children:r}),e&&s.jsx("p",{className:"text-gray-500 text-sm",children:e})]})]});export{c as F};
---
+++ assets/TableData-Swdp7qsk.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-BS_GYby9.js";const r=({children:t,className:s=""})=>a.jsx("td",{className:`px-1 py-2 text-sm text-gray-900 ${s}`,children:t});export{r as T};
